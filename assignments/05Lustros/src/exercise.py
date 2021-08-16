a52d071288c3b378db28a88a7c2a1103d910bdb6
LufeSS <A01742322@itesm.mx>
2021-08-16 23:46:34 +0000

    04 y 05 v2

--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,8 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
-
-
+    nacimiento = int(input("Dame el año de nacimiento: "))
+    actual = int(input("Dame el año actual: "))
+    calculo = (actual-nacimiento)/5
+    print("Los lustros que has vivido son:",calculo)
 
 if __name__ == '__main__':
     main()
