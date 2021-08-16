61772435685ff28ec3d32c06b0c16417a29ebbcc
LufeSS <A01742322@itesm.mx>
2021-08-16 23:29:52 +0000

    04 y 05

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+    edad = int(input("Dame tu edad: "))
+    anoActual = int(input("Dame el año actual: "))
+    cienMenosEdad = int((100)-edad)
+    tendre100en = (anoActual+cienMenosEdad)
+    print("Cumplirás 100 años en el año:",tendre100en)
+
 
 
 
